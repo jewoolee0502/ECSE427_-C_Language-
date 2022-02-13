bbfa9a61f66a7f843ca7c79ed04e7175380edaad
Jewoo Lee <jwcairo0502@gmail.com>
2022-02-13 15:55:06 -0500

    Done Q3, Q4, Q5 of A1

---
+++ Assignment 1/A1/A1-StarterCode/shell.c
@@ -0,0 +1,92 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+#include "interpreter.h"
+#include "shellmemory.h"
+
+
+int MAX_USER_INPUT = 1000;
+int parseInput(char ui[]);
+int oneLiner(char input[]);
+
+// Start of everything
+int main(int argc, char *argv[]) {
+
+	printf("%s\n", "Shell version 1.1 Created January 2022");
+	help();
+
+	char prompt = '$';  				// Shell prompt
+	char userInput[MAX_USER_INPUT];		// user's input stored here
+	int errorCode = 0;					// zero means no error, default
+
+	//init user input
+	for (int i=0; i<MAX_USER_INPUT; i++)
+		userInput[i] = '\0';
+	
+	//init shell memory
+	mem_init();
+
+	while(1) {							
+		printf("%c ",prompt);
+		fgets(userInput, MAX_USER_INPUT-1, stdin);
+
+		if(feof(stdin)) {
+			freopen("/dev/tty", "r", stdin);
+		}
+
+		errorCode = oneLiner(userInput);
+
+		if (errorCode == -1) exit(99);	// ignore all other errors
+		memset(userInput, 0, sizeof(userInput));
+
+	}
+
+	return 0;
+
+}
+
+int oneLiner(char input[]) {
+	char *command = strtok(input, ";");	//split by ;
+
+	while(command != NULL) {
+		if(parseInput(command) != -1) {
+			command = strtok(NULL, ";");
+			continue;
+		}
+		return -1;
+	}
+
+	return 0;
+}
+
+// Extract words from the input then call interpreter
+int parseInput(char ui[]) {
+ 
+	char tmp[200];
+	char *words[100];							
+	int a,b;							
+	int w=0; // wordID
+
+	for(a=0; ui[a]==' ' && a<1000; a++);		// skip white spaces
+
+	while(ui[a] != '\0' && a<1000) {
+
+		for(b=0; ui[a]!='\0' && ui[a]!=' ' && a<1000; a++, b++)
+			tmp[b] = ui[a];						// extract a word
+	 
+		tmp[b] = '\0';
+
+		words[w] = strdup(tmp);
+
+		w++;
+		
+		if(ui[a] == '\0'){
+			break;
+		}
+		a++; 
+	}
+
+	return interpreter(words, w);
+}
